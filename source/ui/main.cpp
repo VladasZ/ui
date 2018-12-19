711e807e5588381a15db215a984dc5dbf803f2cc
Vladas Zakrevskis <146100@gmail.com>
2018-12-19 16:36:31 +0300

    removed console drawer

--- source/ui/main.cpp
+++ source/ui/main.cpp
@@ -11,24 +11,8 @@ using namespace std;
 
 #include "View.hpp"
 
-#include "ui.hpp"
-#include "ConsoleDrawer.hpp"
 
 int main() {
-
-#if !LINUX
-  
-	ui::config::set_drawer(new ui::ConsoleDrawer());
-
-	auto view1 = new ui::View({ 10, 10, 15, 15 });
-	auto view2 = new ui::View({ 3, 3, 10, 10 });
-	auto view3 = new ui::View({ 1, 1, 5, 5 });
-
-	view1->add_subview(view2);
-	view2->add_subview(view3);
-
-	view1->draw();
-#endif
 
 	return 0;
 }
