eb747e617cb4802770533b8b32d75b5e7b5e9d43
Vladas <146100@gmail.com>
2019-12-30 16:21:44 +0300

    fixed standalone build

--- source/ui_test/main.cpp
+++ source/ui_test/main.cpp
@@ -6,9 +6,13 @@
 //  Copyright © 2018 VladasZ. All rights reserved.
 //
 
+#include "Log.hpp"
+#include "View.hpp"
 
 int main() {
 
+    auto view = new ui::View({ 10, 10, 10, 10 });
+    Log(view->frame().to_string());
  
 	return 0;
 }
