0b092e410b2875236ae3cc1a4f78e55670ebad69
Vladas Zakrevskis <146100@gmail.com>
2021-01-12 09:44:38 +0300

    gamepad class

---
+++ source/ui/Input/Gamepad.cpp
@@ -0,0 +1,7 @@
+//
+//  Gamepad.cpp
+//  ui
+//
+//  Created by Vladas Zakrevskis on 12/01/21.
+//  Copyright © 2021 VladasZ. All rights reserved.
+//
---
+++ source/ui/Input/Gamepad.hpp
@@ -0,0 +1,20 @@
+//
+//  Gamepad.hpp
+//  ui
+//
+//  Created by Vladas Zakrevskis on 12/01/21.
+//  Copyright © 2021 VladasZ. All rights reserved.
+//
+
+#pragma once
+
+
+namespace ui {
+
+	class Gamepad {
+
+
+
+	};
+
+}
