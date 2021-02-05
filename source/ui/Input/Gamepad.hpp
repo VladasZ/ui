74c2af7edf89dba1f7ced998332dd43ec93d7873
Vladas Zakrevskis <146100@gmail.com>
2021-02-05 23:36:38 +0300

    gamepad buttons

--- source/ui/Input/Gamepad.hpp
+++ source/ui/Input/Gamepad.hpp
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include "Point.hpp"
+
 
 namespace ui {
 
@@ -15,7 +17,27 @@ namespace ui {
 
 	public:
 
+		static inline bool a;
+		static inline bool b;
+		static inline bool x;
+		static inline bool y;
 
+		static inline bool lb;
+		static inline bool rb;
+
+		static inline bool back;
+		static inline bool start;
+
+		static inline float lt;
+		static inline float rt;
+
+		static inline bool up    = false;
+		static inline bool right = false;
+		static inline bool down  = false;
+		static inline bool left  = false;
+
+		static inline gm::Point left_stick;
+		static inline gm::Point right_stick;
 
 	};
 
